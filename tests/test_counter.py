e4f03ed64e3e5ac963e9667e155e3e154d421326
Snapshot0010 <snapshot00010@gmail.com>
2024-02-12 19:20:09 -0800

    Reforked your repo from previous lab and updated code to try and get CI lab to work

--- tests/test_counter.py
+++ tests/test_counter.py
@@ -10,3 +10,46 @@ how to call the web service and assert what it should return.
 - The service must be able to update a counter by name.
 - The service must be able to read the counter
 """
+from unittest import TestCase
+
+# we need to import the unit under test - counter
+from src.counter import app
+
+# we need to import the file that contains the status codes
+from src import status
+
+
+class CounterTest(TestCase):
+    """Counter tests"""
+    def setUp(self):
+        self.client = app.test_client()
+
+    def test_create_a_counter(self):
+        result = self.client.post('/counters/foo')
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+
+    def test_duplicate_a_counter(self):
+        result = self.client.post('/counters/bar')
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+        result = self.client.post('/counters/bar')
+        self.assertEqual(result.status_code, status.HTTP_409_CONFLICT)
+
+    def test_update_a_counter(self):
+        result = self.client.post('counters/update')
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+        baseline = result.json['update']
+        result = self.client.put('counters/update')
+        self.assertEqual(result.status_code, status.HTTP_200_OK)
+        self.assertEqual(result.json['update'], baseline+1)
+
+    def test_read_a_counter(self):
+        result = self.client.post('counters/read')
+        self.assertEqual(result.status_code, status.HTTP_201_CREATED)
+        result = self.client.get('counters/read')
+        self.assertEqual(result.status_code, status.HTTP_200_OK)
+        self.assertEqual(result.json['read'], 0)
+
+    def test_read_empty_counter(self):
+        result = self.client.get('counters/emptyRead')
+        self.assertEqual(result.status_code, status.HTTP_404_NOT_FOUND)
+        
